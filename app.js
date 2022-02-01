8e01fe734e29e564df4cf004b32b9cc9ea331261
Guido Restaino <dogui256@gmail.com>
2022-01-31 23:02:22 -0300

    aaaah

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ app.get('/registro', (req,res) =>{
     res.sendFile((__dirname + '/views/registro.html'));  
 });
 
-//app.use(express.static(path.join(__dirname, './public'))); 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, './public'))); 
+
 
 app.listen(process.env.PORT || 3030, function () {
     console.log("Servidor corriendo");
